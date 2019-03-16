9882140a6eb24dac73d5e26da354394c226c7181
blacksmith <caltadaniel@gmail.com>
2019-03-16 18:12:40 +0100

    new implementation for piagnolo new

--- piagnolo_telegram_new.py
+++ piagnolo_telegram_new.py
@@ -92,11 +92,18 @@ class telegram_thread(threading.Thread):
                     if float(next_req.args[0]) > 15 and float(next_req.args[0]) < 24:
                         self.actual_setpoint = float(next_req.args[0])
                         self.heater_enabled = True
-                        publish.single("home/sala/stufa", "1", hostname=hostname, port=1883)
+                        publish.single("home/sala/rele1", "1", hostname=hostname, port=1883)
                     else:
                         self.actual_setpoint = self.default
                         self.heater_enabled = False
-                        publish.single("home/sala/stufa", "0", hostname=hostname, port=1883)
+                        publish.single("home/sala/rele1", "0", hostname=hostname, port=1883)
+                if next_req.name == "home/sala/stufa_reset":
+                    prog_log.debug('Replying to heater reset request to {}'.format(next_req.chat_id))
+                    publish.single("home/sala/rele2", "1", hostname=hostname, port=1883)
+                    prog_log.debug('reset command 1')
+                    time.sleep(1)
+                    publish.single("home/sala/rele2", "0", hostname=hostname, port=1883)
+                    prog_log.debug('reset command 0')
                 if next_req.name == "home/sala/grafico":
                     fig,ax1 = plt.subplots()
                     ax1.plot(self.temp_time, self.temp, 'b-o')
@@ -114,6 +121,8 @@ class telegram_thread(threading.Thread):
                     plt.savefig('temp.png')
                     self.bot.send_photo(chat_id=next_req.chat_id, photo=open('temp.png','rb'))
                     prog_log.debug('Replying to plot request to {}'.format(next_req.chat_id))
+                
+
             queueLock.acquire()
             if not queue_to_telegram.empty():
                 # a new message is available on the queue
@@ -125,14 +134,6 @@ class telegram_thread(threading.Thread):
                     val = msg.payload.decode('utf-8')
                     numeric_val = float(val)
                     if msg.topic == 'home/sala/temperature':
-                        #temp control
-                        if self.heater_enabled:
-                            if numeric_val < self.actual_setpoint:
-                                # turn on
-                                publish.single("home/sala/stufa", "1", hostname=hostname, port=1883)
-                            else:
-                                # turn off
-                                publish.single("home/sala/stufa", "0", hostname=hostname, port=1883)
                         self.last_temperature_sala = numeric_val
                         self.temp.append(numeric_val)
                         self.temp_time.append(datetime.datetime.now())
@@ -160,4 +161,155 @@ class mqtt_thread(threading.Thread):
         self.queue_to_telegram = queue_to_telegram
         self.queue_to_mqtt = queue_to_mqtt
 
+    def on_connect(self,  client, userdata, flags, rc):
+        # print("Connected with result code " + str(rc))
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe("home/#")
+        prog_log.info('Connected to mqtt server')
 
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(self, client, userdata, msg):
+        if DEBUG:
+            print(msg.topic + " " + str(msg.payload))
+        # prog_log.debug('Received message {} from {}'.format(str(msg.payload), msg.topic))
+        queueLock.acquire()
+        queue_to_telegram.put(msg)
+        queueLock.release()
+
+    def run(self):
+        client = mqtt.Client()
+        client.on_connect = self.on_connect  # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.on_message = self.on_message
+        client.connect(hostname, 1883, 60)
+
+        # Blocking call that processes network traffic, dispatches callbacks and
+        # handles reconnecting.
+        # Other loop*() functions are available that give a threaded interface and a
+        # manual interface.
+        client.loop_forever()
+
+
+class TelegramBarsanti:
+    def __init__(self, token, to_telegram_queue):
+        self.token = token
+        self.to_telegram_queue = to_telegram_queue
+        self.updater = Updater(token)
+        self.updater.dispatcher.add_handler(CommandHandler('start', self.start))
+        self.updater.dispatcher.add_handler(CommandHandler('help', self.help))
+        self.updater.dispatcher.add_handler(MessageHandler(Filters.text, self.generic_msg))
+        self.tg_thread = telegram_thread(self.to_telegram_queue)
+        self.tg_thread.start()
+
+    def keyboard(self, bot, update):
+        custom_keyboard = [['Sala on', 'Sala off','Reset sala'], ['Camera on', 'Camera off', 'Reset camera'], ['Temperatura sala', 'Grafico sala']]
+        reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+        bot.send_message(chat_id=update.message.chat.id, text="Seleziona il comando", reply_markup=reply_markup)
+
+    def start(self, bot, update):
+        update.message.reply_text('Benvenuto al centro di controllo piagnolo')
+        self.keyboard(bot, update)
+        prog_log.debug('Received start request from telegram')
+
+
+    def temperature(self, bot, update):
+        temp_req = Request("/home/sala/temperature", bot, update.message.chat.id, None)
+        requestLock.acquire()
+        request_queue.put(temp_req)
+        requestLock.release()
+        # self.bot.send_message(chat_id=self.chat_id, text="Trying to measure the actual temperature")
+        prog_log.debug('Received temperature request from telegram')
+
+    def plot(self, bot, update):
+        temp_req = Request("home/sala/grafico", bot, update.message.chat.id, None)
+        requestLock.acquire()
+        request_queue.put(temp_req)
+        requestLock.release()
+        # self.bot.send_message(chat_id=self.chat_id, text="Trying to measure the actual temperature")
+        prog_log.debug('Received plot request from telegram')
+
+    def turn_on_heater(self, bot, update, val):
+        prog_log.debug("Stufa ON")
+        update.message.reply_text('Accensione stufa sala')
+        stufa_req = Request("home/sala/stufa", bot, update.message.chat.id, ["23.0"])
+        requestLock.acquire()
+        request_queue.put(stufa_req)
+        requestLock.release()
+
+    def turn_off_heater(self, bot, update):
+        prog_log.debug("Stufa OFF")
+        update.message.reply_text('Spegnimento stufa sala')
+        stufa_req = Request("home/sala/stufa", bot, update.message.chat.id, ["0.0"])
+        requestLock.acquire()
+        request_queue.put(stufa_req)
+        requestLock.release()
+
+    def reset_sala(self, bot, update):
+        prog_log.debug("Coamdo reset sala")
+        update.message.reply_text('Spegnimento stufa sala')
+        stufa_req = Request("home/sala/stufa_reset", bot, update.message.chat.id, [])
+        requestLock.acquire()
+        request_queue.put(stufa_req)
+        requestLock.release()
+
+
+    def help(self, bot, update):
+        helpString = 'Benvenuto nel centro di controllo della casa.\n ' \
+                     'I comandi disponibili sono: ' \
+                     '1)\\stufaON'
+        update.message.reply_text(helpString)
+
+
+    def generic_msg(self,bot, update):
+        #bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+        if update.message.text == "Sala on":
+            self.setpoint(bot,update)
+        elif update.message.text == "Sala off":
+            self.turn_off_heater(bot,update)
+        elif update.message.text == "Reset sala":
+            self.reset_sala(bot,update)
+        elif update.message.text == "Grafico sala":
+            self.plot(bot,update)
+        elif update.message.text == "Temperatura sala":
+            self.temperature(bot,update)
+        elif update.message.chat_id == self.last_chat_id:
+            if self.last_request == "setpoint":
+                # we have the new setpoint
+                self.setpoint_val = float(update.message.text)
+                #text_d = "The new setpoint is {}".format(self.setpoint_val)
+                #bot.send_message(chat_id=update.message.chat_id, text=text_d)
+                self.turn_on_heater(bot, update, self.setpoint_val)
+                self.keyboard(bot, update)
+
+    def run(self):
+        self.updater.start_polling(timeout=30, read_latency=10)
+        self.updater.idle()
+
+def getopts(argv):
+    opts = {}  # Empty dictionary to store key-value pairs.
+    while argv:  # While there are arguments left to parse...
+        if argv[0][0] == '-':  # Found a "-name value" pair.
+            opts[argv[0]] = argv[1]  # Add key and value to the dictionary.
+        argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
+    return opts
+
+def main():
+
+    from sys import argv
+    myargs = getopts(argv)
+    if '-t' in myargs:  # Example usage.
+        print(myargs['-t'])
+        token = myargs['-t']
+        to_mqtt_Queue = queue.Queue(10)
+        mqtt_thr = mqtt_thread(to_mqtt_Queue, queue_to_telegram)
+        mqtt_thr.start()
+        myTgBar = TelegramBarsanti(token, queue_to_telegram)
+        myTgBar.run()
+    else:
+        print('Wrong args')
+
+
+
+if __name__ == '__main__':
+    main()
